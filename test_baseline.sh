5f6205e61b1ff5ac1f6d3eeb6a7bcce3e2748913
rd211 <david.dinucujianu@gmail.com>
2024-02-16 19:21:08 +0100

    removed testing notebook and updated test path

--- test_baseline.sh
+++ test_baseline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 test.py --config rvt_test.json --checkpoint ./mlruns/228219311864833537/2ac5cf4d764f45bfa84d9424f241abd7/artifacts/model_best_ep47_val_loss_0.0105.pth
+python3 test.py --config rvt_2_layered_test.json --checkpoint [PATH]
